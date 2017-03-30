842f6b3c83c200d942e048ec817015afc0531176
Jimmy Zong <jzong@palantir.com>
2017-03-30 13:56:28 -0400

    refactor 49

--- tdd-exercises/java/string-calculator/src/main/resources/Conversion.java
+++ tdd-exercises/java/string-calculator/src/main/resources/Conversion.java
@@ -3,14 +3,11 @@
  */
 public class Conversion {
     static String digit[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",  "X"};
+    static String tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
 
     public static String parse(int i) {
 
-        String result = "";
-        for (int j = 0; j < i / 10; j++) {
-            result += "X";
-        }
-        if (i > 40) result = "XL";
+        String result = tens[i / 10];
         return result + digit[i%10];
     }
 }
