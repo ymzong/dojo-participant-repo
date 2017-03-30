769ff47ff513994589d967f39ec3c6fdca175909
Jimmy Zong <jzong@palantir.com>
2017-03-30 13:44:41 -0400

    1 to 10

--- tdd-exercises/java/string-calculator/src/main/resources/Conversion.java
+++ tdd-exercises/java/string-calculator/src/main/resources/Conversion.java
@@ -2,9 +2,9 @@
  * Created by jzong on 3/30/17.
  */
 public class Conversion {
-    static String digit[] = {"I", "II", "III", "IV"};
+    static String digit[] = {"zero", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",  "X"};
 
     public static String parse(int i) {
-        return digit[i-1];
+        return digit[i];
     }
 }
